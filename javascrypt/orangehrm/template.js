ba0ef060dff228e34b414be278c7be04121556f3
Christian Calisto Fernandes de Oliveira <ccfo2002@gmail.com>
2023-11-09 19:40:20 +0000

    Mudei coisas

--- javascrypt/orangehrm/template.js
+++ javascrypt/orangehrm/template.js
@@ -4,14 +4,4 @@ async function name() {
 
     //Abrir o navegador
     let driver = await new Builder().forBrowser("chrome").build(); //Pedir para o código construir um driver do navegador escolhido;
-
-    //Navegar até o site
-    await driver.get("https://lambdatest.github.io/sample-todo-app/");
-    //Adicionar uma tarefa
-
-    //Fechar o navegador
-
-
-
-
 }
