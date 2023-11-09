e8661a62c8814ea73d5d40cb9300abfeda8b7225
Christian Calisto Fernandes de Oliveira <ccfo2002@gmail.com>
2023-11-09 19:57:02 +0000

    Fiz os testes com o selenium puro

--- javascrypt/todolist/todolist.js
+++ javascrypt/todolist/todolist.js
@@ -1,17 +1,25 @@
-const {Builder} = require ("selenium-webdriver")
+const {Builder, Key, By} = require ("selenium-webdriver")
+const assert = require ("assert")
 
-async function name() {
+async function todolist() {
 
-    //Abrir o navegador
+    //Abrir o navegador;
     let driver = await new Builder().forBrowser("chrome").build(); //Pedir para o código construir um driver do navegador escolhido;
 
-    //Navegar até o site
+    //Navegar até o site;
     await driver.get("https://lambdatest.github.io/sample-todo-app/");
-    //Adicionar uma tarefa
+
+    //Adicionar uma tarefa;
+    await driver.findElement(By.id("sampletodotext")).sendKeys("Aprender Selenium", Key.RETURN);
+
+    //Assertion - Verificar se adicionou a tarefa requisitada;
+    let seleniumText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText().then(function(value){
+        return value
+    });
+    assert.strictEqual(seleniumText, "Aprender Selenium");
 
     //Fechar o navegador
-
-
-
+    await driver.close();
 
 }
+todolist();
