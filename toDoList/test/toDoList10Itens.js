d718f1ac35f10415a7816a8a76a11f1c1b389182
Christian Calisto Fernandes de Oliveira <ccfo2002@gmail.com>
2023-11-10 21:13:08 +0000

    Projeto Final
    A to do list está completa com três formas diferentes de automatizar os testes. Usando o selenium webdriver, mocha e chai para ser feito as linhas de código;

--- toDoList/test/toDoList10Itens.js
+++ toDoList/test/toDoList10Itens.js
@@ -5,7 +5,7 @@ var should = require("chai").should();
 describe("todolist", function () {
 
     //it Block
-    it("Adicionou a tarefa com sucesso", async function () {
+    it("Adicionou as tarefas com sucesso", async function () {
         //Abrir o navegador;
         let driver = await new Builder().forBrowser("chrome").build(); //Pedir para o código construir um driver do navegador escolhido;
 
@@ -24,13 +24,29 @@ describe("todolist", function () {
         await driver.findElement(By.id("sampletodotext")).sendKeys("Selenium com Java", Key.RETURN);
         await driver.findElement(By.id("sampletodotext")).sendKeys("API", Key.RETURN);
 
-        /*Assertion - Verificar se adicionou a tarefa requisitada;*/
-        let seleniumText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText().then(function (value) {
-            return value
-        });
+        /*Assertion - Verificar se adicionou as tarefas requisitadas;*/
+        let seleniumText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText().then(function (value) {return value});
+        let cypressText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[7]/span")).getText().then(function (value) {return value});
+        let javascryptText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[8]/span")).getText().then(function (value) {return value});
+        let javaText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[9]/span")).getText().then(function (value) {return value});
+        let htmlText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[10]/span")).getText().then(function (value) {return value});
+        let cssText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[11]/span")).getText().then(function (value) {return value});
+        let gitText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[12]/span")).getText().then(function (value) {return value});
+        let jiraText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[13]/span")).getText().then(function (value) {return value});
+        let seleniumjavaText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[14]/span")).getText().then(function (value) {return value});
+        let apiText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[15]/span")).getText().then(function (value) {return value});
 
         //Assertion Chai Should - Verificar se adicionou a tarefa requisitada;
-        seleniumText.should.equal("Aprender Selenium")
+        seleniumText.should.equal("Selenium")
+        cypressText.should.equal("Cypress")
+        javascryptText.should.equal("JavaScrypt")
+        javaText.should.equal("Java")
+        htmlText.should.equal("HTML5")
+        cssText.should.equal("CSS3")
+        gitText.should.equal("Git e GitHub")
+        jiraText.should.equal("Jira")
+        seleniumjavaText.should.equal("Selenium com Java")
+        apiText.should.equal("API")
 
         //Fechar o navegador
         await driver.close();
