c90897f353c71a932308bc4101a79f9ad396748e
Christian Calisto Fernandes de Oliveira <ccfo2002@gmail.com>
2023-11-10 20:15:59 +0000

    Adicionei outro script

--- javascrypt/todolist/todolist.js
+++ javascrypt/todolist/todolist.js
@@ -1,31 +1,31 @@
 const { Builder, Key, By } = require("selenium-webdriver");
-const assert = require("assert");
 var should = require("chai").should();
 
-async function todolist() {
+//Describe block
+describe("todolist", function () {
 
-    //Abrir o navegador;
-    let driver = await new Builder().forBrowser("chrome").build(); //Pedir para o código construir um driver do navegador escolhido;
+    //it Block
+    it("Adicionou a tarefa com sucesso", async function () {
+        //Abrir o navegador;
+        let driver = await new Builder().forBrowser("chrome").build(); //Pedir para o código construir um driver do navegador escolhido;
 
-    //Navegar até o site;
-    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+        //Navegar até o site;
+        await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
-    //Adicionar uma tarefa;
-    await driver.findElement(By.id("sampletodotext")).sendKeys("Aprender Selenium", Key.RETURN);
+        //Adicionar uma tarefa;
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Aprender Selenium", Key.RETURN);
 
-    //Assertion - Verificar se adicionou a tarefa requisitada;
-    let seleniumText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText().then(function (value) {
-        return value
+        //Assertion - Verificar se adicionou a tarefa requisitada;
+        let seleniumText = await driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText().then(function (value) {
+            return value
+        });
+
+        //Assertion Chai Should - Verificar se adicionou a tarefa requisitada;
+        seleniumText.should.equal("Aprender Selenium")
+
+        //Fechar o navegador
+        await driver.close();
+
     });
 
-    /*Assertion Node Puro 
-    assert.strictEqual(seleniumText, "Aprender Selenium");*/
-
-    //Assertion Chai Should - Verificar se adicionou a tarefa requisitada;
-    seleniumText.should.equal("Aprender Selenium")
-
-    //Fechar o navegador
-    await driver.close();
-
-}
-todolist();
+});
